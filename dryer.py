c916489d293e5957d633c456b068639bdf2bc009
Geoff Gallaway <github@geoffeg.org>
2020-04-23 22:26:46 +0100

    Write to a database implemented, use python library.

--- dryer.py
+++ dryer.py
@@ -1,8 +1,9 @@
+import sys
 import time
 import json
 import requests
 import os
-import postgresql
+import psycopg2
 from collections import deque
 from statistics import variance
 
@@ -13,26 +14,52 @@ class Dryer:
   state_time = 0
   notification_sent = False 
   readings = deque(maxlen=10)
+  poll_interval = 60 # seconds
+  last_db_insert_time = 0
 
   def __init__(self):
     self.state_time = time.time()
 
-  def write_reading(self, reading):
-    pg_user = os.environ['PG_USER']
-    pg_pass = os.environ['PG_PASS']
-    self.db = postgresql.open(f'pg://{pg_user}:{pg_pass}@192.168.86.186/sensors')
-    insert = self.db.prepare("INSERT into readings (device_id, sensor_id, reading) VALUES((SELECT id from devices where name = 'dryer'), (SELECT id from sensors where name = 'accelerometer_x'), $1)")
-    insert(reading)
+  def write_readings(self, ax, ay, az, mx, my, mz, gx, gy, gz):
+    sensor_name_map = {
+      'accelerometer_x' : ax,
+      'accelerometer_y' : ay,
+      'accelerometer_z' : az,
+      'magnetometer_x'  : mx,
+      'magnetometer_y'  : my,
+      'magnetometer_z'  : mz,
+      'gyroscope_x'     : gx,
+      'gyroscope_y'     : gy,
+      'gyroscope_z'     : gz
+    }
+
+    if (self.dryer_state == "off" and time.time() > self.last_db_insert_time + 60) or self.dryer_state == "on":
+      self.last_db_insert_time = time.time()
+   
+      attempts = 0
+      while attempts < 3:
+        try:
+          if not hasattr(self, 'db') or self.db is None:
+            self.db = psycopg2.connect("dbname=sensors host=192.168.86.186 port=6543")
+          cursor = self.db.cursor()
+          insert = cursor.executemany("INSERT into readings (device_id, sensor_id, reading) VALUES((SELECT id from devices where name = 'dryer'), (SELECT id from sensors where name = %s), %s)", list(sensor_name_map.items()))
+          self.db.commit()
+          break
+        except Exception as e:
+         attempts += 1
+         print(e)
 
   def dryer_off(self):
     current_time = time.time()
     if current_time > self.transition_time + 60:
       time_string = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(self.transition_time))
       if self.notification_sent == False and self.previous_dryer_state == "on" and self.state_time > 600 :
+        self.poll_interval = 60
         self.send_notification()
         self.notification_sent = True
 
   def dryer_on(self):
+    self.poll_interval = 60
     self.notification_sent = False
 
   def send_notification(self):
@@ -52,11 +79,13 @@ class Dryer:
     self.state_time = time.time() - self.transition_time
     self.transition_time = time.time()
 
-  def add_reading(self, reading):
-    self.readings.append(reading)
+  def add_reading(self, ax, ay, az, gx, gy, gz, mx, my, mz):
+    ax = ax * 1000
+    self.readings.append(ax)
+    self.write_readings(ax, ay, az, gx, gy, gz, mx, my, mz)
     if len(self.readings) == 10:
       readings_variance = int(variance(self.readings))
-      print(f'{reading} {readings_variance}')
+      print(f'{ax} {readings_variance}')
       if readings_variance > 500:
         if self.dryer_state != "on":
           self.transition()
@@ -68,4 +97,4 @@ class Dryer:
           self.set_state("off")
         self.dryer_off()
     else:
-      print(reading)
+      print(ax)
--- sensor.py
+++ sensor.py
@@ -1,32 +1,17 @@
 import smbus
 import time
+from icm20948 import ICM20948
 from dryer import Dryer
 
-def init_imu(bus):
-  bus.write_byte_data(0x68, 0x7F, 0x00) # Select bank 0
-  bus.write_byte_data(0x68, 0x06, 0x80) # Reset
-  time.sleep(0.1)
-  bus.write_byte_data(0x68, 0x06, 0x01) # Run mode
-  bus.write_byte_data(0x68, 0x7F, 0x20) # Select bank 2
-  bus.write_byte_data(0x68, 0x14, 0x30 | 0x00 | 0x01) # 2g modee
-  bus.write_byte_data(0x68, 0x7F, 0x00) # Select bank 0
-
-def get_reading(bus):
-  x_high = bus.read_byte_data(0x68, 0x2D)
-  x_low = bus.read_byte_data(0x68, 0x2E)
-  x = (x_high<<8)|x_low
-  if x>=32767:
-    x=x-65535
-  elif x<=-32767:
-    x=x+65535
-  return x
-
 if __name__ == '__main__':
-  bus = smbus.SMBus(1)
-  init_imu(bus)
+  icm20948 = ICM20948()
+ 
 
   dryer = Dryer()
   while True:
-    reading = get_reading(bus)
-    dryer.add_reading(reading)
+    ax, ay, az, gx, gy, gz = icm20948.read_accelerometer_gyro_data()
+    mx, my, mz = icm20948.read_magnetometer_data()
+    t = int(time.time())
+    print(f'{t} {ax} {ay} {az} {gx} {gy} {gz} {mx} {my} {mz}')
+    dryer.add_reading(ax, ay, az, gx, gy, gz, mx, my, mz)
     time.sleep(1)
